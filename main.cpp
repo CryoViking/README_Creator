e62bda740802c31374e9aba0938fff7f1a25da25
PotatoMaster101 <PotatoMaster101@users.noreply.github.com>
2020-08-10 13:11:42 +0800

    Progress!

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
-std::vector<std::string> parseArgumentLine(){
+bool grabFileNamesRecursively(std::string path){
+    return true;
+}
 
+void performActions(std::vector<std::string> actions){
+    for(auto i = actions.begin(); i != actions.end(); ++ i){
+        switch((*i)[0]){
+            case 'p':
+                std::cout << *i << std::endl;
+                const std::string& chars = "\t\n\v\f\r ";
+                (*i).erase((*i).find_first_not_of(chars));
+                (*i).erase((*i).find_last_not_of(chars) + 1);
+                std::cout << *i << std::endl;
+                //grabFileNamesRecursively();
+                break;
+        }
+    }
+}
+
+std::vector<std::string> parseArgumentString(std::string argument_string){
+    std::stringstream string_to_split(argument_string);
+    std::string segment;
+    std::vector<std::string> seglist;
+
+    while(std::getline(string_to_split, segment, '-')){
+        seglist.push_back(segment);
+    }
+
+    return seglist;
 }
 
 int main(int argc, char* argv[]){
@@ -21,6 +49,14 @@ int main(int argc, char* argv[]){
         argument_string += ' ';
     }
 
+    std::vector<std::string> parsed_argument_list = parseArgumentString(argument_string);
+    parsed_argument_list.erase(parsed_argument_list.begin());
+
+    //for(auto i = parsed_argument_list.begin(); i != parsed_argument_list.end(); ++ i){
+    //    std::cout << *i << std::endl;
+    //}
+
+    performActions(parsed_argument_list);
 
 
     return 0;
